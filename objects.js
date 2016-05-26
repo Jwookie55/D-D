57d58e808c59a060c4b0e53d575eb32f17b7ed94
Jared Stevens <Jwookie55@live.com>
2016-05-26 11:43:21 -0700

    Add files via upload
    just a start need to look at passing something besides just a string for the traits.

---
+++ objects.js
@@ -0,0 +1,140 @@
+Races{
+	dwarf: {
+	  desc: "Short and stout the dwarves are bold and hardy and are known as skilled warriors.  As wide as they are tall a dwarf often waieghs as much as a human standing 2 feet taller.  They have long lives often living to 400 years and so carry grudges far longer than any human might.  They live in clans which band together to form knigdoms, living underground where they mine the minerals and riches they covet and turn them into wonders.  An adventuring dwarf is oten motivated by a desire for treasure.  Though many are either inspired or commanded by a diety to bring glory."
+	  abilityMod: {constitution: 2},
+	  age: "Dwarves mature at the same rate as humans, but they're considered young until they reach the age of 50. On average they live about 350 years.",
+	  align: "Most dwarves are lawful, believing firmly in the benefits of a well-ordered society.  They tend toward good as well, with a strong sense of fair play and a belief that everyone deserves to share in the benefits of a just order.",
+	  size: "medium",
+	  speed: {num: 25 ,mod: "Your speed is not reduced by wearing heavy armor."},
+	  darkVision: true,
+	  traits: {dwarven_resilience: "You have advantage on saving throws against poison, and you have a resistance against posion damage.", 
+	  			dwarven_combat_training: "You have the proficiency with the battleaxe, handaxe, light hammer and war hammer",
+	  			tool_proficiency: "You gain proficiency with the artisian's tools of your choice: smith's tools, brewers supplies or masons tools"
+			},
+	  languages: ["common", "dwarvish"]
+	  subraces: {
+	  	hill: {abilityMod: {wisdom: 1} ,traits: {dwarven_toughness: "Your hit point maxium increases by 1 and it increases by 1 every time you level"}},
+	  	mount: {abilityMod: {strength: 2},traits: {dwarven_armor_training: "You have proficiency with light and medium armor."}}
+	  	}
+	},
+	elf: {
+	  desc: //string
+	  abilityMod: //{stat: number}
+	  age: //string
+	  align: //string
+	  size: //sting
+	  speed: //{num: ,mod:}
+	  darkVision: //boolean
+	  traits: //{trait name: desc}
+	  languages: //[list of languages]
+	  subraces: {
+	  	hill: {abilityMod: ,traits:},
+	  	mount: {abilityMod: ,traits:}
+	  	}
+	},
+	halfling: {
+	  desc: //string
+	  abilityMod: //{stat: number}
+	  age: //string
+	  align: //string
+	  size: //sting
+	  speed: //{num: ,mod:}
+	  darkVision: //boolean
+	  traits: //{trait name: desc}
+	  languages: //[list of languages]
+	  subraces: {
+	  	hill: {abilityMod: ,traits:},
+	  	mount: {abilityMod: ,traits:}
+	  	}
+	},
+	human: {
+	  desc: //string
+	  abilityMod: //{stat: number}
+	  age: //string
+	  align: //string
+	  size: //sting
+	  speed: //{num: ,mod:}
+	  darkVision: //boolean
+	  traits: //{trait name: desc}
+	  languages: //[list of languages]
+	  subraces: {
+	  	hill: {abilityMod: ,traits:},
+	  	mount: {abilityMod: ,traits:}
+	  	}
+	},
+	dragonborn: {
+	  desc: //string
+	  abilityMod: //{stat: number}
+	  age: //string
+	  align: //string
+	  size: //sting
+	  speed: //{num: ,mod:}
+	  darkVision: //boolean
+	  traits: //{trait name: desc}
+	  languages: //[list of languages]
+	  subraces: {
+	  	hill: {abilityMod: ,traits:},
+	  	mount: {abilityMod: ,traits:}
+	  	}
+	},
+	gnome: {
+	  desc: //string
+	  abilityMod: //{stat: number}
+	  age: //string
+	  align: //string
+	  size: //sting
+	  speed: //{num: ,mod:}
+	  darkVision: //boolean
+	  traits: //{trait name: desc}
+	  languages: //[list of languages]
+	  subraces: {
+	  	hill: {abilityMod: ,traits:},
+	  	mount: {abilityMod: ,traits:}
+	  	}
+	},
+	halfelf: {
+	  desc: //string
+	  abilityMod: //{stat: number}
+	  age: //string
+	  align: //string
+	  size: //sting
+	  speed: //{num: ,mod:}
+	  darkVision: //boolean
+	  traits: //{trait name: desc}
+	  languages: //[list of languages]
+	  subraces: {
+	  	hill: {abilityMod: ,traits:},
+	  	mount: {abilityMod: ,traits:}
+	  	}
+	},
+	halforc: {
+	  desc: //string
+	  abilityMod: //{stat: number}
+	  age: //string
+	  align: //string
+	  size: //sting
+	  speed: //{num: ,mod:}
+	  darkVision: //boolean
+	  traits: //{trait name: desc}
+	  languages: //[list of languages]
+	  subraces: {
+	  	hill: {abilityMod: ,traits:},
+	  	mount: {abilityMod: ,traits:}
+	  	}
+	},
+	tiefling: {
+	  desc: //string
+	  abilityMod: //{stat: number}
+	  age: //string
+	  align: //string
+	  size: //sting
+	  speed: //{num: ,mod:}
+	  darkVision: //boolean
+	  traits: //{trait name: desc}
+	  languages: //[list of languages]
+	  subraces: {
+	  	hill: {abilityMod: ,traits:},
+	  	mount: {abilityMod: ,traits:}
+	  	}
+	},
+}
